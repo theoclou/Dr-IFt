400eabf76468d3de95d3d8d968813a5405d7df49
a1bderrahmane <abderrahmaneetudier@gmail.com>
2024-12-15 17:20:06 +0100

    random

--- app/interface.py
+++ app/interface.py
@@ -1,4 +1,11 @@
 import streamlit as st
+import sys
+import os
+
+# Add the parent directory to sys.path
+parent_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
+if parent_dir not in sys.path:
+    sys.path.insert(0, parent_dir)
 from car_search.sparql_manager import SparqlManager
 import random
 import matplotlib.pyplot as plt
